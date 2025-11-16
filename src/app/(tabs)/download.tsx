4d2276fdfe7d05cd46c26a65585ade6a5dec0e50
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2025-11-16 23:04:32 +0100

    Extract hook

--- src/app/(tabs)/download.tsx
+++ src/app/(tabs)/download.tsx
@@ -26,36 +26,7 @@ import { humanFileSize } from '@/utils/common';
 import { messageForException } from '@/utils/error';
 import { KeyDerivationAlgorithm } from '@/utils/password';
 import { runCatching } from '@/utils/result';
-
-function useExpirationTime(expirationDate: Date | null): `${string}:${string}` | 'expired' | null {
-  const [expiresIn, setExpiration] = React.useState<`${string}:${string}` | 'expired' | null>(null);
-
-  React.useEffect(() => {
-    if (!expirationDate) {
-      setExpiration(null);
-      return;
-    }
-
-    const interval = setInterval(() => {
-      const now = new Date();
-      const duration = Math.floor((expirationDate.getTime() - now.getTime()) / 1000);
-
-      if (duration <= 0) {
-        setExpiration('expired');
-        clearInterval(interval);
-        return;
-      }
-
-      const minutes = Math.floor(duration / 60).toLocaleString(undefined, { minimumIntegerDigits: 2 });
-      const seconds = (duration % 60).toLocaleString(undefined, { minimumIntegerDigits: 2 });
-      setExpiration(`${minutes}:${seconds}`);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, [expirationDate]);
-
-  return expiresIn;
-}
+import { useExpirationTime } from '@/hooks/use-expiration-time';
 
 type DownloadStep = 'load' | 'decrypt' | 'display';
 
---
+++ src/hooks/use-expiration-time.ts
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+export function useExpirationTime(expirationDate: Date | null): `${string}:${string}` | 'expired' | null {
+  const [expiresIn, setExpiration] = React.useState<`${string}:${string}` | 'expired' | null>(null);
+
+  React.useEffect(() => {
+    if (!expirationDate) {
+      setExpiration(null);
+      return;
+    }
+
+    const interval = setInterval(() => {
+      const now = new Date();
+      const duration = Math.floor((expirationDate.getTime() - now.getTime()) / 1000);
+
+      if (duration <= 0) {
+        setExpiration('expired');
+        clearInterval(interval);
+        return;
+      }
+
+      const minutes = Math.floor(duration / 60).toLocaleString(undefined, { minimumIntegerDigits: 2 });
+      const seconds = (duration % 60).toLocaleString(undefined, { minimumIntegerDigits: 2 });
+      setExpiration(`${minutes}:${seconds}`);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [expirationDate]);
+
+  return expiresIn;
+}
